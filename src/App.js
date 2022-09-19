1453a1aba4aba0934ec732c3c0daf236cab97a19
sufian <sufian4se@gmail.com>
2022-09-19 23:33:47 +0600

    load dynamic data, API call useEffect integrate state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
+}
+
+function ExternalUsers () {
+  const [users, setUsers] = useState([]);
+  // useEffect( () => {}, []);
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, []);
+  return (
+    <div>
+      <h2>External Users</h2>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props) {
+  return (
+    <div style={{border: '2px solid red', margin: '20px'}}>
+      <h3>Name: {props.name}</h3>
+      <p>Email: {props.email}</p>
+    </div>
+  )
 }
 
 function Counter() {
